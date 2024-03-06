ec248ffc67deea497f3e40ea27754bea4351c133
daxidngyn <daxidnguyen@gmail.com>
2024-03-05 16:09:47 -0800

    [feat] add simple price chart
    Add simple price chart
    - using TradingView widget, planning on implementing with custom chart later

--- src/components/simple-price/chart.tsx
+++ src/components/simple-price/chart.tsx
@@ -1,7 +1,7 @@
 "use client";
 
+import React, { useEffect, useState, useRef, memo } from "react";
 import { cn } from "@/utils/cn";
-import { useState } from "react";
 
 const periods = ["1H", "24H", "7D", "1M", "3M", "6M", "1Y", "ALL"];
 
@@ -10,7 +10,7 @@ export default function SimplePriceChart() {
 
   return (
     <div>
-      <div>
+      <div className="mb-3">
         <div className="text-[#0B1426] font-semibold text-sm">
           Bitcoin Price Chart (USD)
         </div>
@@ -30,6 +30,52 @@ export default function SimplePriceChart() {
           ))}
         </div>
       </div>
+
+      <TradingViewWidget />
     </div>
   );
 }
+
+const TradingViewWidget = memo(() => {
+  const container = useRef();
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src =
+      "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
+    script.type = "text/javascript";
+    script.async = true;
+    script.innerHTML = `
+        {
+          "autosize": true,
+          "symbol": "BITSTAMP:BTCUSD",
+          "interval": "D",
+          "timezone": "Etc/UTC",
+          "theme": "light",
+          "style": "1",
+          "locale": "en",
+          "enable_publishing": false,
+          "hide_top_toolbar": true,
+          "hide_legend": true,
+          "allow_symbol_change": true,
+          "calendar": false,
+          "support_host": "https://www.tradingview.com"
+        }`;
+    container.current.appendChild(script);
+  }, []);
+
+  return (
+    <div
+      className="tradingview-widget-container"
+      ref={container}
+      style={{ height: "100%", width: "100%" }}
+    >
+      <div
+        className="tradingview-widget-container__widget"
+        style={{ height: "calc(100% - 32px)", width: "100%" }}
+      ></div>
+    </div>
+  );
+});
+
+TradingViewWidget.displayName = "TradingView Widget";
